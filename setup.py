f3449aa042b7368f7c81f205558b9636f875b19b
Jing Dai <flyflyjean@Jings-MacBook-Pro.local>
2016-11-27 17:45:43 -0800

    tested locally

---
+++ setup.py
@@ -0,0 +1,18 @@
+try:
+    from setuptools import setup, Extension, Command
+except ImportError:
+    from distutils.core import setup, Extension, Command
+    
+setup(
+        name="CalCalc",
+        version=0.0.1,
+        description="'calculate' that evaluates any string passed to it"
+        author="Jing Dai",
+        author_email="jdai@berkeley.edu",
+        url="https://github.com/flyflyjean/JingDai_AY250_HW10.git",
+        license = 'None',
+        packages=['calcalc'],
+        install_requires = ['bs4', 'urllib', 'argparse', 'pytest']
+        setup_requires=['pytest-runner'],
+        tests_require=['pytest']
+)
